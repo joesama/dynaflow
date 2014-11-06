7fb703cfecc4e7bec6f7d7ddc5546d6419e84a31
sohib <sohib@javan.co.id>
2014-11-06 09:23:31 +0700

    migrate flow step

---
+++ src/migrations/2014_11_06_021635_create_sys_flow_step_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSysFlowStepTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sys_flow_step', function($table){
+			$table->increments('id');
+			$table->foreign('sys_flow_id')->references('id')->on('sys_flow');
+			$table->string('name')->default(null);
+			$table->string('action')->default(null);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+		Schema::drop('sys_flow_step');
+	}
+
+}
