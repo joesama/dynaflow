b4e501dfb91a484c28e7ff96d5015fc00f3e7aec
sohib <sohib@javan.co.id>
2014-11-25 11:04:11 +0700

    cearte class migration application and form manager

---
+++ src/migrations/2014_11_25_034602_create_application_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateApplicationTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sys_application', function($table){
+			$table->increments('id');
+			$table->integer('flow_id')->references('id')->on('sys_flow');
+			$table->string('name')->default(null);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sys_application');
+	}
+
+}
