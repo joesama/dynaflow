2a2b219505a20bbd1e93a9c4d54e7acf8a52e464
nurrohman <nurrohman@javan.co.id>
2014-11-27 12:01:56 +0700

    enhancement formmanager

--- src/Javan/Dynaflow/FormBuilder/SysFormManagerForm.php
+++ src/Javan/Dynaflow/FormBuilder/SysFormManagerForm.php
@@ -12,17 +12,35 @@ class SysFormManagerForm extends Form
                 'empty_value' => '',
                 'multiple' => false
             ])
+            
             ->add('Flow Step', 'choice', [
                 'choices' => [1 => 'Makan'],
                 'empty_value' => '',
                 'multiple' => false
             ])
         	
-        	->add('Name', 'text')
+            ->add('Title', 'text')
+            
+            ->add('Type', 'choice', [
+                'choices' => [1 => 'Makan'],
+                'empty_value' => '',
+                'multiple' => false
+            ])
+        	
+            ->add('Name', 'text')
+
+            ->add('Value', 'text')
+
+            // ->add('gender', 'choice', [
+            //     'choices' => [1 => 'Yes', 2 => 'Female'],
+            //     'selected' => 1,
+            //     'expanded' => true
+            // ])
 
         	->add('save', 'submit', [
                 'attr' => ['class' => 'btn btn-primary']
             ])
+            
             ->add('clear', 'reset', [
                 'label' => 'Reset',
                 'attr' => ['class' => 'btn btn-danger']
